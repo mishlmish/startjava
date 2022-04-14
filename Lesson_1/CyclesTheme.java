06bd1cd5ed7ad9b561c4c6ec8ea745b6e7ff9a0a
mishlmish <mishlmog@gmail.com>
2022-04-14 20:46:59 +0300

    вторая версия CyclesTheme.java

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -5,130 +5,118 @@ class CyclesTheme {
 
         int fromNum = -10;
         int toNum = 21;
-        int number = fromNum;
-        int oddNumSum = 0;
-        int evenNumSum = 0;
+        int counter = fromNum;
+        int sumOdd = 0;
+        int sumEven = 0;
 
         do {
-            if (number % 2 == 0) {
-                evenNumSum += number;
+            if (counter % 2 == 0) {
+                sumEven += counter;
             } else {
-                oddNumSum += number;
+                sumOdd += counter;
             }
-            number++;
-        } while (number <= toNum);
+            counter++;
+        } while (counter <= toNum);
 
         System.out.println("\nСумма четных чисел от " + fromNum + " до " + toNum +
-                " равна " + evenNumSum);
+                " равна " + sumEven);
         System.out.println("Сумма нечетных чисел от " + fromNum + " до " + toNum + " равна " +
-                oddNumSum);
+                sumOdd);
 
         //2. Вывод чисел между min  и max
         System.out.println("\n2. Вывод чисел между min  и max");
 
-        int var1 = 10;
-        int var2 = 5;
-        int var3 = -1;
-        int maxNum = 0;
-        int minNum = 0;
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = -1;
+        int maxNum = num1;
+        int minNum = num2;
 
-        if (var1 >= var2 && var1 >= var3) {
-            maxNum = var1;
+        if (num1 >= num2 && num1 >= num3) {
+            maxNum = num1;
 
-            if (var2 >= var3) {
-                minNum = var3;
+            if (num3 < num2) {
+                minNum = num3;
             } else {
-                minNum = var2;
+                minNum = num2;
             }
-        } else if (var2 >= var1 && var2 >= var3) {
-            maxNum = var2;
+        } else if (num2 >= num1 && num2 >= num3) {
+            maxNum = num2;
 
-            if (var1 >= var3) {
-                minNum = var3;
+            if (num3 < num1) {
+                minNum = num3;
             } else {
-                minNum = var1;
+                minNum = num1;
             }
-        } else  if (var3 >= var1 && var3 >= var2) {
-            maxNum = var3;
+        } else  if (num3 >= num1 && num3 >= num2) {
+            maxNum = num3;
 
-            if (var2>= var3) {
-                minNum = var3;
+            if (num3 < num2) {
+                minNum = num3;
             } else {
-                minNum = var2;
+                minNum = num2;
             }
         }
 
-        System.out.print("\nВсе числа в диапазоне от " + minNum + " до " + maxNum+ " :   ");
+        System.out.print("\nВсе числа между " + minNum + " до " + maxNum+ " :   ");
 
-        for ( int i = minNum; i <= maxNum; i++) {
-            System.out.print("" + i + "  ");
+        for (int i = minNum + 1; i < maxNum; i++) {
+            System.out.print(i + "  ");
         }
 
         //3. Вывод реверсивного числа и суммы его цифр
         System.out.println("\n\n3. Вывод реверсивного числа и суммы его цифр");
 
         int srcNum = 1234;
-        int digit1 = -1;
-        int digit2 =-1;
-        int digit3 = -1;
-        int digit4 = -1;
+        int sum = srcNum % 10;
         int tempNum = srcNum; 
+        int inversNum = (srcNum % 10) * 1000 ;
+        int decMultiplier = 1000;
 
         while (tempNum > 0) {
-            if (digit1 == -1) {
-                digit1 = srcNum % 10;
-                tempNum = srcNum / 10;
-            } else if (digit2 == -1) {
-                digit2 = tempNum % 10;
-                tempNum = tempNum / 10;
-            } else if (digit3 == -1) {
-                digit3 = tempNum % 10;
-                tempNum = tempNum / 10;
-            } else if (digit4 == -1) {
-                digit4 = tempNum % 10;
-                tempNum = tempNum / 10;
-            }
+            tempNum = tempNum / 10;
+            sum = sum + tempNum % 10;
+            decMultiplier = decMultiplier / 10;
+            inversNum = inversNum + (tempNum % 10) * decMultiplier;
         }
 
-        int inversNum = digit1 * 1000 + digit2 * 100 + digit3 * 10 + digit4;
         System.out.println("\nЧисло " + srcNum + " в обратном порядке будет числом: " + inversNum +
-                " , сумма его цифр: " + (digit4 + digit3 + digit2 + digit1));
+                " , сумма его цифр: " + sum);
 
         //4. Вывод чисел на консоль в несколько строк
-        System.out.println("\n4. Вывод чисел на консоль в несколько строк");
+        System.out.println("\n4. Вывод чисел на консоль в несколько строк\n");
 
-        int srcLeft = 1;
-        int srcRight = 24;
+        fromNum = 1;
+        toNum = 24;
+        int zeroNum = (5 - ((toNum - fromNum + 1) / 2) % 5) * 2;
 
-        for (int i = srcLeft - 1; i < (srcRight / 5) + 1; i += 2) {
-            System.out.println();
-            for (int j = 0 ; j < 10; j += 2) {
-                int tempIndex = i * 5 + j + 2;
-
-                if (tempIndex < srcRight) {
-                System.out.printf(" %2d", tempIndex);
-
+        for (int i = fromNum; i < toNum + zeroNum; i += 2) {
+            if (i < toNum) {
+                if ((i - fromNum) % 10 != 8){
+                    System.out.printf("%s%3d", "   ", i);
                 } else {
-                System.out.printf(" %2d", 0);
+                    System.out.printf("%s%3d\n", "   ", i);
                 }
+            } else {
+                if (zeroNum != 10) {
+                System.out.printf("%s%3d", "   ", 0);
+                                }
             }
-            System.out.println();
         }
 
         //5. Проверка количества единиц на четность
-        System.out.println("\n5. Проверка количества единиц на четность");
+        System.out.println("\n\n5. Проверка количества единиц на четность");
 
         srcNum = 3141591;
         tempNum = srcNum;
         int numOfOnes = 0;
 
         while (tempNum > 0) {
-
             if (tempNum % 10 == 1) {
-            numOfOnes++;
+                numOfOnes++;
             }
 
-            tempNum = tempNum / 10;
+            tempNum /= 10;
         }
 
         System.out.print("\nКоличество единиц в числе " + srcNum + " : " + numOfOnes + " , ");
@@ -144,7 +132,7 @@ class CyclesTheme {
         //Квадрат
         int hight = 5;
 
-        while (hight > 0) {
+            while (hight > 0) {
             int width = 10;
 
             System.out.printf("\n%10s", " ");
@@ -165,7 +153,7 @@ class CyclesTheme {
         }
 
         //Треугольник
-        int lengthProfile = 12321 ;
+        int lengthProfile = 12321;
 
         System.out.println();
 
@@ -185,7 +173,7 @@ class CyclesTheme {
 
         System.out.printf("\n%6s%s", " ", "Code Char");
 
-        for (int i = 1; i < 128; i +=2) {
+        for (int i = 1; i < 128; i += 2) {
             System.out.printf("\n%5s%4d%5c", " ", i, (char) i);
         }
 
@@ -193,8 +181,30 @@ class CyclesTheme {
         System.out.println("\n\n8. Проверка, является ли число палиндромом");
 
         srcNum = 12321;
+        tempNum = srcNum;
+        int digitNum = 0;
 
-        if (srcNum / 10000 == srcNum % 10 && (srcNum % 10000) / 1000 == (srcNum % 100) / 10) {
+        while (tempNum > 0) {
+            tempNum = tempNum / 10;
+            digitNum++;
+        }
+
+        decMultiplier = 1;
+
+       for (int i = 1; i < digitNum; i++) {
+            decMultiplier *=10;
+        }
+
+        tempNum = srcNum;
+        inversNum = 0;
+
+        for (int j = 0; j < digitNum; j++) {
+            inversNum += (tempNum % 10) * decMultiplier;
+            decMultiplier /= 10;
+            tempNum /= 10;
+        }
+
+        if (srcNum == inversNum) {
             System.out.println("\nЧисло " + srcNum + " является палиндромом");
         } else {
             System.out.println("\nЧисло " + srcNum + " не является палиндромом");
@@ -203,19 +213,28 @@ class CyclesTheme {
         //9. Определение, является ли число счастливым
         System.out.println("\n9. Определение, является ли число счастливым\n");
 
-        srcNum = 356842;
-        int firstThreeNum = srcNum / 1000;
-        int secondThreeNum = srcNum % 1000;
+        srcNum = 356841;
+        int leftDigits = srcNum / 1000;
+        int rigthDigits = srcNum % 1000;
+        int leftSum = 0;
+        int rightSum = 0;
 
-        int firstSum = firstThreeNum / 100 + (firstThreeNum % 100) /10 + firstThreeNum % 10;
-        int secondSum = secondThreeNum / 100 + (secondThreeNum % 100) /10 + secondThreeNum % 10;
+        for (int i = 0; i < 3; i++) {
+            leftSum += leftDigits % 10;
+            leftDigits /= 10;
+            rightSum += rigthDigits % 10;
+            rigthDigits /= 10;
+        }
 
-        System.out.println("В числе " + srcNum + " сумма цифр первой тройки " + firstThreeNum +
-                " = " + firstSum);
-        System.out.println("сумма цифр второй тройки " + secondThreeNum +
-                " = " + secondSum);
+        leftDigits = srcNum / 1000;
+        rigthDigits = srcNum % 1000;
 
-        if (firstSum == secondSum) {
+        System.out.println("В числе " + srcNum + " сумма цифр первой тройки " + leftDigits +
+                " = " + leftSum);
+        System.out.println("сумма цифр второй тройки " + rigthDigits +
+                " = " + rightSum);
+
+        if (leftSum == rightSum) {
             System.out.println("число является счастливым");
         } else {
             System.out.println("число не является счастливым");
@@ -234,8 +253,8 @@ class CyclesTheme {
         System.out.println();
 
         for (int i = 2; i < 10; i++) {
-            System.out.printf("%2d%s%4d%4d%4d%4d%4d%4d%4d%4d\n", i , " |", i*2 , i*3, i*4, i*5, +
-                    i*6, i*7, i*8, i*9);
+            System.out.printf("%2d%s%4d%4d%4d%4d%4d%4d%4d%4d\n", i , " |", i * 2 , i * 3, i * 4, +
+                    i * 5, i * 6, i * 7, i * 8, i * 9);
         }
     }
 }
