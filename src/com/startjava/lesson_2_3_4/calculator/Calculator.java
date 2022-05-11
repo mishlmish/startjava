ffd54c3b3c787a73988ab36170cb48ff7a7904ea
mishlmish <mishlmog@gmail.com>
2022-05-11 03:34:25 +0300

    4й урок первая версия калькулятора со звездочкой Calculator.java  CalculatorTest.java

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,34 +2,19 @@ package com.startjava.lesson_2_3_4.calculator;
 
 class Calculator {
 
-    private String inputExpression;
-
-    public Calculator(String inputExpression) {
-        this.inputExpression = inputExpression;
-    }
-
-    public float calculate() {
-        String[] splitedData = inputExpression.split(" ");
-        int num1 = Integer.parseInt(splitedData[0]);
-        char sign = splitedData[1].charAt(0);
-        int num2 = Integer.parseInt(splitedData[2]);
-
-        switch (sign) {
-            case '+' :
-                return Math.addExact(num1, num2);
-            case '-' :
-                return Math.subtractExact(num1, num2);
-            case '*' :
-                return Math.multiplyExact(num1, num2);
-            case '/' :
-                return (float) num1 / (float) num2;
-            case '%' :
-                return num1 % num2;
-            case '^' :
-                return (float) Math.pow((double) num1, (double) num2);
-            default :
+    public static float calculate(int num1, char sign, int num2) {
+        float value = switch (sign) {
+            case '+' -> Math.addExact(num1, num2);
+            case '-' -> Math.subtractExact(num1, num2);
+            case '*' -> Math.multiplyExact(num1, num2);
+            case '/' -> (float) num1 / (float) num2;
+            case '%' -> num1 % num2;
+            case '^' -> (float) Math.pow((double) num1, (double) num2);
+            default ->   {
                 System.out.println("\nВведите правильный знак математической операции");
-                return 0;
-        }
+                yield 0;
+            }
+        };
+        return value;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -3,24 +3,47 @@ package com.startjava.lesson_2_3_4.calculator;
 import java.util.Scanner;
 
 class CalculatorTest {
-    
+
     public static void main(String[] args) {
         String answer;
         Scanner console = new Scanner(System.in);
 
+        String[] splitedData;
+        String inputExpression;
+        boolean numeric;
+
         do {
-            System.out.print("\nВведите выражение в формате число  пробел знак пробел число: ");
-            String inputExpression = console.nextLine();
+            numeric = true;
 
-            Calculator calc = new Calculator(inputExpression);
+            System.out.print("\nВведите выражение в формате: [положительное целое число]  пробел [знак] пробел ");
+            System.out.println("[положительное целое число]");
+            inputExpression = console.nextLine();
+            splitedData = inputExpression.split(" ");
 
-            System.out.println(inputExpression + " = " + calc.calculate());
+            if (splitedData.length == 3 && splitedData[1].length() == 1) {
+                for (int i = 0; i < 3; i = i + 2) {
+                    for (int j = 0; j < splitedData[i].length(); j++) {
+                        numeric = numeric && splitedData[i].charAt(j) > 47 && splitedData[i].charAt(j) < 58;
+                    }
+                }
+
+                if (numeric && Integer.parseInt(splitedData[0]) * Integer.parseInt(splitedData[2]) != 0) {
+                    System.out.println(inputExpression + " = " +
+                            Calculator.calculate(Integer.parseInt(splitedData[0]),
+                                    splitedData[1].charAt(0), Integer.parseInt(splitedData[2])));
+                } else {
+                    System.out.println(" Введите положительные числа");
+                }
+            } else {
+                System.out.println("Вы ввели некорректные данные");
+            }
 
             do {
                 System.out.println("\nПродолжить вычисления? [yes/no] : ");
                 answer = console.next();
-            } while(!answer.equals("yes") && !answer.equals("no"));
+            } while (!answer.equals("yes") && !answer.equals("no"));
             console.nextLine();
-        } while(!answer.equals("no"));
+
+        } while (!answer.equals("no"));
     }
 }
