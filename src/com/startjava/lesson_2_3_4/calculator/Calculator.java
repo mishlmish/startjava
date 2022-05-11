b31e8c8fbfda8fea31bb0d89697f9a18baa6484e
mishlmish <mishlmog@gmail.com>
2022-05-11 15:58:36 +0300

    первое исправление *Калькулятор 4-го урока Calculator.java  CalculatorTest.java

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,19 +2,45 @@ package com.startjava.lesson_2_3_4.calculator;
 
 class Calculator {
 
-    public static float calculate(int num1, char sign, int num2) {
-        float value = switch (sign) {
-            case '+' -> Math.addExact(num1, num2);
-            case '-' -> Math.subtractExact(num1, num2);
-            case '*' -> Math.multiplyExact(num1, num2);
-            case '/' -> (float) num1 / (float) num2;
-            case '%' -> num1 % num2;
-            case '^' -> (float) Math.pow((double) num1, (double) num2);
-            default ->   {
-                System.out.println("\nВведите правильный знак математической операции");
-                yield 0;
+    public static boolean computed;
+
+    public static float calculate(String inputExpression) {
+        String[] splitedData = inputExpression.split(" ");
+        boolean numeric = true;
+        computed = true;
+
+        if (splitedData.length == 3 && splitedData[1].length() == 1) {
+            for (int i = 0; i < 3; i = i + 2) {
+                for (int j = 0; j < splitedData[i].length(); j++) {
+                    numeric = numeric && splitedData[i].charAt(j) > 47 && splitedData[i].charAt(j) < 58;
+                }
             }
-        };
-        return value;
+
+            if (numeric && Integer.parseInt(splitedData[0]) * Integer.parseInt(splitedData[2]) != 0) {
+
+                int num1 = Integer.parseInt(splitedData[0]);
+                char sign = splitedData[1].charAt(0);
+                int num2 = Integer.parseInt(splitedData[2]);
+
+                return switch (sign) {
+                    case '+' -> Math.addExact(num1, num2);
+                    case '-' -> Math.subtractExact(num1, num2);
+                    case '*' -> Math.multiplyExact(num1, num2);
+                    case '/' -> (float) num1 / (float) num2;
+                    case '%' -> num1 % num2;
+                    case '^' -> (float) Math.pow((double) num1, (double) num2);
+                    default -> {
+                        System.out.println("\nВведите правильный знак математической операции");
+                        yield 0;
+                    }
+                };
+            } else {
+                computed = false;
+                return 0;
+            }
+        } else {
+            computed = false;
+            return 0;
+        }
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -8,32 +8,15 @@ class CalculatorTest {
         String answer;
         Scanner console = new Scanner(System.in);
 
-        String[] splitedData;
         String inputExpression;
-        boolean numeric;
 
         do {
-            numeric = true;
-
             System.out.print("\nВведите выражение в формате: [положительное целое число]  пробел [знак] пробел ");
             System.out.println("[положительное целое число]");
             inputExpression = console.nextLine();
-            splitedData = inputExpression.split(" ");
 
-            if (splitedData.length == 3 && splitedData[1].length() == 1) {
-                for (int i = 0; i < 3; i = i + 2) {
-                    for (int j = 0; j < splitedData[i].length(); j++) {
-                        numeric = numeric && splitedData[i].charAt(j) > 47 && splitedData[i].charAt(j) < 58;
-                    }
-                }
-
-                if (numeric && Integer.parseInt(splitedData[0]) * Integer.parseInt(splitedData[2]) != 0) {
-                    System.out.println(inputExpression + " = " +
-                            Calculator.calculate(Integer.parseInt(splitedData[0]),
-                                    splitedData[1].charAt(0), Integer.parseInt(splitedData[2])));
-                } else {
-                    System.out.println(" Введите положительные числа");
-                }
+            if ((Calculator.calculate(inputExpression) != 0 || Calculator.computed) == true) {
+                System.out.println(inputExpression + " = " + Calculator.calculate(inputExpression));
             } else {
                 System.out.println("Вы ввели некорректные данные");
             }
